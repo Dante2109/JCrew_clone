ab53456b70a947898c5c80bdd02c9dc6fbdc8a58
rakesh-pasupathy <reachmerakeshpasupathy@gmail.com>
2022-11-11 21:15:32 +0530

    footer import/export added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import{navbar,transitions} from "../enormous-step-3337/Components/navbar.js"
 document.getElementById("navbar").innerHTML = navbar();
+
+import footer from "../enormous-step-3337/Components/footer.js"
+document.getElementById("footer").innerHTML = footer();
+
 transitions();
