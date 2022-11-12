5e5aec4a86be6afdfa485b25b98f771e19185df6
chukkapraveen <chukkapraveenkumar303@gmail.com>
2022-11-12 22:17:06 +0530

    Changed

--- scripts/womens.js
+++ scripts/womens.js
@@ -3,38 +3,38 @@ var womensData = [
     {
       image:
         "https://www.jcrew.com/s7-img-facade/AS297_GR7081_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-        description:"NEW COLOR",
-        name: "Vintage long-sleeve T-shirt",
+        name:"New Shirt",
+        description: "Vintage long-sleeve T-shirt",
       price:  2696,
       
     },
     {
       image:
         "https://www.jcrew.com/s7-img-facade/BF355_PK6709_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-        description:"NEW COLOR",
-         name: "Slim perfect-fit T-shirt",
+        name:"New Shirt",
+        description: "Slim perfect-fit T-shirt",
         price: 4146,
     },
     {
       image:
         "https://www.jcrew.com/s7-img-facade/BH925_RD5371_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-        description:"BEST SELLER",
-        name: "New favorite tank top ",
+        name:"BEST SELLER",
+        description: "New favorite tank top ",
       price: 1399,
     },
     {
       image:
         "https://www.jcrew.com/s7-img-facade/BL022_RD5745?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=480&hei=480",
-        description:"NEW COLOR",
-        name: "Tissue turtleneck",
+        name:"New Shirt",
+        description: "Tissue turtleneck",
       price: 4699,
 
     },
     {
       image:
         "https://www.jcrew.com/s7-img-facade/BM342_GR7105_d2?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-        description:"BEST COLOR",
-        name: "Perfect-fit turtleneck ",
+        name:"BEew Shirt",
+        description: "Perfect-fit turtleneck ",
       price: 10499,
       type:"women"
 
@@ -42,8 +42,8 @@ var womensData = [
     {
       image:
         "https://www.jcrew.com/s7-img-facade/BM948_NA7294?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=480&hei=480",
-        description:"NEW COLOR",
-         name: "New chateau parka ",
+        name:"New Shirt",
+        description: "New chateau parka ",
       price: 21999,
       type:"women"
    
@@ -51,8 +51,8 @@ var womensData = [
     {
       image:
         "https://www.jcrew.com/s7-img-facade/BL022_WT0002_d1?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-        description:"NEW  COLOR",
-       name: "Tissue turtleneck",
+        name:"NEew Shirt",
+        description: "Tissue turtleneck",
       price: 7699,
       type:"women"
  
@@ -60,135 +60,135 @@ var womensData = [
     {
       image:
         "https://www.jcrew.com/s7-img-facade/BC108_BK0001?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=480&hei=480",
-        description:"NEW COLOR",
-        name: "Ruched one-shoulder one-piece",
+        name:"New Shirt",
+        description: "Ruched one-shoulder one-piece",
       price: 7299,
       type:"women"
     },
     {
         image:
           "https://www.jcrew.com/s7-img-facade/BK251_BK0001_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "Kate straight-leg pant",
+          name:"New Shirt",
+          description: "Kate straight-leg pant",
         price: 13299,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/BM534_NA6434_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "New Perfect Winter parka",
+          name:"New Shirt",
+          description: "New Perfect Winter parka",
         price: 23999,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/BK728_RD6052_d2?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "Sequin mini skirt",
+          name:"New Shirt",
+          description: "Sequin mini skirt",
         price: 3299,
         type:"women"
       },
       {
         image:"https://www.jcrew.com/s7-img-facade/BM897_SR0399_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "Fair Isle sweater-skirt",
+        name:"New Shirt",
+        description: "Fair Isle sweater-skirt",
         price: 7999,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/BM779_NA6434_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "Ribbed pom-pom beanie",
+          name:"New Shirt",
+          description: "Ribbed pom-pom beanie",
         price: 13299,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/BM299_NA6821?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=480&hei=480",
-          description:"NEW COLOR",
-          name: "Half-zip stretch wool sweater",
+          name:"New Shirt",
+          description: "Half-zip stretch wool sweater",
         price: 13299,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/BM538_WT0002_d7?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "New chateau puffer coat",
+          name:"New Shirt",
+          description: "New chateau puffer coat",
         price: 13299,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/BM165_BL6622_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "Cable-knit rollneck sweater",
+          name:"New Shirt",
+          description: "Cable-knit rollneck sweater",
         price: 13299,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/BM692_EE3244?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "Colette mule heels",
+          name:"New Shirt",
+          description: "Colette mule heels",
         price: 13299,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/BD613_SU3958_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "Rollneck sweater in Supersoft yarn",
+          name:"New Shirt",
+          description: "Rollneck sweater in Supersoft yarn",
         price: 13299,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/H1891_EE1584?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=480&hei=480",
-          description:"NEW COLOR",
-          name: "Perfect Winter boots with shearling",
+          name:"New Shirt",
+          description: "Perfect Winter boots with shearling",
         price: 13299,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/BM179_SR0404_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "Fair Isle mockneck pullover sweater",
+          name:"New Shirt",
+          description: "Fair Isle mockneck pullover sweater",
         price: 13299,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/BM301_SR0554_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "Cable-knit mockneck pullover in stripe with",
+          name:"New Shirt",
+          description: "Cable-knit mockneck pullover in stripe with",
         price: 13299,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/BM838_RD5371_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "Florence convertible clutch ",
+          name:"New Shirt",
+          description: "Florence convertible clutch ",
         price: 13299,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/BN125_GR7084_d2?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "Velvet jeweled button-fron",
+          name:"New Shirt",
+          description: "Velvet jeweled button-fron",
         price: 13299,
         type:"women"
       },
       {
         image:
           "https://www.jcrew.com/s7-img-facade/BM349_BK0001_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540",
-          description:"NEW COLOR",
-          name: "Feather-trim long-sleeve shirt",
+          name:"New Shirt",
+          description: "Feather-trim long-sleeve shirt",
         price: 13299,
         type:"women"
       },
